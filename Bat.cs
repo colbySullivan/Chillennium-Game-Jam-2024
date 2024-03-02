affda00f67af44f7a9f7a264616c134e991b1b9d
Colby Sullivan <sullivanc@southwestern.edu>
2024-03-01 22:53:32 -0600

    Bat mob

---
+++ Bat.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class Bat : CharacterBody2D
+{
+	public const float Speed = 5.0f;
+	private AnimatedSprite2D _animatedSprite;
+	
+	public CharacterBody2D _Ouro;
+	
+	public Vector2 delayedPos;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+
+	public override void _Ready()
+		{
+			
+			_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+			_Ouro = GetParent().GetNode<CharacterBody2D>("Ouro");
+			base._Ready();
+		}
+	public override void _PhysicsProcess(double delta)
+	{
+		_animatedSprite.Play("default");
+		//Vector2 velocity = Velocity;
+		//velocity.X -=  Speed;
+		//Velocity = velocity;
+		if(Position.X > delayedPos.X)
+			_animatedSprite.FlipH = false;
+		else
+			_animatedSprite.FlipH = true;
+		Position += (delayedPos - Position) / 40;
+		//MoveAndSlide();
+	}
+	private void _on_area_2d_body_entered(Node2D body)
+	{
+		if(body.Name == "Ouro")
+			GetTree().ChangeSceneToFile("res://menuLevel/menu.tscn");
+	}
+	private void _on_timer_timeout()
+	{
+		delayedPos = _Ouro.Position;
+	}
+}
---
+++ level1/level1.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class level1 : Node2D
+{
+	public CharacterBody2D _Ouro;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_Ouro = GetParent().GetNode<CharacterBody2D>("Ouro");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+	
+	private void _on_despawn_timer_timeout()
+	{
+		QueueFree();
+	}
+	
+	private void _on_spawn_timer_timeout()
+	{
+		var scene = GD.Load<PackedScene>("res://player/bat.tscn");
+		var instance = scene.Instantiate();
+		//instance.Positon.X = _Ouro.Position.X + 10;
+		AddChild(instance);
+	}
+}
